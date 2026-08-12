6317b1d4247832a54ba8800d213215e01403588d
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2026-08-12 13:28:54 -0500

    + fix for emits for col.component

--- _components/index.vue
+++ _components/index.vue
@@ -221,7 +221,7 @@
                       v-if="(comp = col.component(props.row))"
                       :is="comp.is"
                       v-bind="comp.props"
-                      v-on="comp?.emits"
+                      v-on="comp?.emits ?? {}"
                     />
                   </template>
                   <div v-if="!col?.component">
